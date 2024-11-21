6d31c76e90dcb75e68a03720d692e635c046b127
sstokes <sterlingjstokes@gmail.com>
2024-11-21 01:44:19 -0500

    Reactored the code, adding constructor function to create a student based on their id. Each student has properties such as  id, simple avg, weighted avg, and list of submitted assignments. The submitted assignment property is a combo of some of the properties learner submission and assignment group

--- sba.js
+++ sba.js
@@ -12,16 +12,16 @@ const AssignmentGroup = {
   group_weight: 25,
   assignments: [
     {
-      id: 1,
-      name: "Declare a Variable",
-      due_at: "2023-01-25",
-      points_possible: 50,
-    },
-    {
       id: 2,
       name: "Write a Function",
       due_at: "2023-02-27",
       points_possible: 150,
+    },
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50,
     },
     {
       id: 3,
@@ -101,60 +101,85 @@ function getLearnerData(course, ag, submissions) {
     //   }
   ];
 
-  // here, we would process this data to achieve the desired result.
-  // console.log(submissions);
 
   //Which learner are we talking about
   let studentIDs = getLearnerIds(getData,submissions);
-  //console.log(studentIDs);
-  //Calculate the weighted average for each student
+  let students = []; //array of the students class
 
-  let studentScores = [];
-  
-  for(let submission of submissions){
-    let id = 125;//!hard coded
-    if(submission.learner_id == id)
-    {
-      let assignment = {
-        learner_id: id,
-        assignment_id: submission.assignment_id,
-        weighted_grade: 0        
-      }
-      Object.assign(assignment, submission.submission); //copies properties from one object to another in this case we get the LearnerSubmission submission object(the internal object)
-      studentScores.push(assignment);
-    }
+  for(let id of studentIDs) // All the students in the course
+  {
+    let submittedAssignments = getSubmissionForStudent(submissions,ag, id);
+    // console.log(submittedAssignments);
+    // console.log(`found all of student: ${id} comepleted assignments`);
+    // let assignment = {};
+    // Object.assign(assignment, submittedAssignments.submission);
+    // Object.assign(assignment, ag.submission); //copies properties from one object to another in this case we get the LearnerSubmission submission object(the internal object)
+    //studentScores.push(assignment);
+    let student = new Student(id,0,0,[],submittedAssignments)
+    students.push(student);
   }
 
-  console.log(studentScores); //multidimensional array [{}]  SubmittedAssignments based on learner
-  //?Can we put into a function
-  for(let i = 0, assign = ag.assignments; i < studentScores.length; i++){
-    totalPossiblePoints = 700 //!hard coded can we pass the total 
-    studentScores.forEach((assignment) => {
-       if(assignment.assignment_id == assign[i].id){
-        let simpleAvg =  assignment.score / assign[i].points_possible; // grades with no weigth
-        let weight = assign[i].points_possible / totalPossiblePoints; // weight an assignment holds
-        assignment.weighted_grade = Math.trunc((simpleAvg * weight)*100); // the individual grade (weigthed) as a whole number
-        //The Math.trunc() static method returns the integer part of a number by removing any fractional digits. //!rounds down
-      }
-    })
+  //console.log(students[0].submittedAssignments);
 }
+//     //Calculate the weighted average for each student
+//   for(let submission of submissions){
+//     let id = 125;//!hard coded
+//     if(submission.learner_id == id)
+//     {
+//       let assignment = {
+//         learner_id: id,
+//         assignment_id: submission.assignment_id,
+//         weighted_grade: 0        
+//       }
+//       Object.assign(assignment, submission.submission); //copies properties from one object to another in this case we get the LearnerSubmission submission object(the internal object)
+//       studentScores.push(assignment);
+//     }
+//   }
+
+//   console.log(studentScores); //multidimensional array [{}]  SubmittedAssignments based on learner
+//   //?Can we put into a function
+//   for(let i = 0, assign = ag.assignments; i < studentScores.length; i++){
+//     totalPossiblePoints = 700 //!hard coded can we pass the total 
+//     studentScores.forEach((assignment) => {
+//        if(assignment.assignment_id == assign[i].id){
+//         let simpleAvg =  assignment.score / assign[i].points_possible; // grades with no weigth
+//         let weight = assign[i].points_possible / totalPossiblePoints; // weight an assignment holds
+//         assignment.weighted_grade = Math.trunc((simpleAvg * weight)*100); // the individual grade (weigthed) as a whole number
+//         //The Math.trunc() static method returns the integer part of a number by removing any fractional digits. //!rounds down
+//       }
+//     })
+// }
 
   
-weightedavg = studentScores.reduce(function(sum, element){ //method returns a single value: the function's accumulated result based on the equation below
-  return sum + element.weighted_grade;
-},0);
-console.log(weightedavg); 
+// weightedavg = studentScores.reduce(function(sum, element){ //method returns a single value: the function's accumulated result based on the equation below
+//   return sum + element.weighted_grade;
+// },0);
+// console.log(weightedavg); 
 
 
-
-
-
-
-
-  return result;
-}
 
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+
+
+/**
+ * 
+ * @param {int} id 
+ * @param {float} simpleAvg  
+ * @param {float} weightedAvg 
+ * @param {array} courses 
+ * @param {array} submittedAssignments 
+ */
+function Student(id, simpleAvg, weightedAvg, courses, submittedAssignments )
+{
+  this.id = id, 
+  this.simpleAvg = simpleAvg, 
+  this.weightedAvg = weightedAvg, 
+  this.courses = courses,
+  this.submittedAssignments =submittedAssignments;
+}
+
+
+
 
 function getLearnerIds(resultArray, learnerSubmissions) {
   for(let submission of learnerSubmissions)
@@ -166,4 +191,35 @@ function getLearnerIds(resultArray, learnerSubmissions) {
   return resultArray; //Return the array of all the students in the course
 }
 
-//console.log(result);
+/**
+ * 
+ * @param {array} submissions - Total submissions by the student
+ * @param {array} assignmentGroup - all the assignments for the course 
+ * @param {integer} id - Student Id 
+ * @returns [{},...] That represents the all student's submitted assignments
+ */
+function getSubmissionForStudent(submissions, assignmentGroup, id){
+  assignmentGroup.assignments.sort(function(a,b){
+    if(a.id < b.id)
+      return -1; //A negative value indicates that a should come before b.
+    else if(a.id > b.id)
+      return 1;  //A positive value indicates that a should come after b.
+    return  0; //Zero or NaN indicates that a and b are considered equal -> stay the same
+  });
+  let coursework = assignmentGroup.assignments;
+  let learnerSubmissions = submissions.filter((submission) => submission.learner_id == id);
+  let result = [];
+  //loop through learner submissions
+  for(let sub of learnerSubmissions)
+  {
+    let assignmentDetails = coursework.find((assignment) => assignment.id === sub.assignment_id)
+    if(assignmentDetails === undefined) // Somehow the learner submitted a assignment that was not in the coursework
+      continue;
+    let obj = {};
+    Object.assign(obj,assignmentDetails,sub.submission);
+    result.push(obj);
+  }
+  return result;
+}
+
+
